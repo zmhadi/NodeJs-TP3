10039ec63b3cb1cef581ce4d8d93d723d0372642
Zmhadi <zakariya.mhadi@gmail.com>
2022-03-29 13:33:46 +0200

    Add lock for body request

--- src/controllers/user-routes.js
+++ src/controllers/user-routes.js
@@ -7,13 +7,14 @@ router.get('/', (req, res) => {
   res.send(userRepository.getUsers())
 });
 
-router.post('/login', body('firstName').not().isEmpty() ,
-                      body('password').not().isEmpty() ,
+router.post('/login', body('firstName').not().isEmpty().withMessage('FirstName is empty !') ,
+                      body('password').not().isEmpty().withMessage('Password is empty !') ,
                       (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).send(errors.array().map(element => element.msg))
   }
+
   if(userRepository.isAuthentified(req.body) == false) {
     res.status(401).send("Login failed !")
   }
@@ -29,10 +30,15 @@ router.get('/:firstName', (req, res) => {
   res.send(foundUser);
 });
 
-router.post('/', body('firstName').not().isEmpty() ,
-                 body('lastName').not().isEmpty() ,
-                 body('password').isLength({ min: 8 }),
+router.post('/', body('firstName').not().isEmpty().withMessage('FirstName is empty !') ,
+                 body('lastName').not().isEmpty().withMessage('LastName is empty !') ,
+                 body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 chars long !'),
                  (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors.array().map(element => element.msg))
+  }
+  
   if(userRepository.isAdmin(req.headers.authorization)) {
     userRepository.createUser(req.body);
     res.status(201).send("User created !");
@@ -40,12 +46,20 @@ router.post('/', body('firstName').not().isEmpty() ,
   res.status(401).send("No permission !")
 });
 
-router.put('/:id', (req, res) => {
-  if(userRepository.isAdmin(req.headers.authorization)) {
-    userRepository.updateUser(req.params.id, req.body);
-    res.status(204).send("Modification saved !");
+router.put('/:id', body('firstName').not().isEmpty(),
+                   body('lastName').not().isEmpty(), 
+                   body('password').isLength({ min: 8 }),
+                   body('roles').not().isEmpty(), 
+                   (req, res) => {
+  const errors = validationResult(req);
+  if (errors.array().length < 2) {
+    if(userRepository.isAdmin(req.headers.authorization)) {
+      userRepository.updateUser(req.params.id, req.body);
+      res.status(204).send("Modification saved !");
+    }
+    res.status(401).send("No permission !")
   }
-  res.status(401).send("No permission !")
+  return res.status(400).send("You must update one element !")
 });
 
 router.delete('/:id', (req, res) => {
