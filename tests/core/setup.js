fdf9b0f52193f6f0c86854ef71d67f01aee34114
zmhadi <93578078+zmhadi@users.noreply.github.com>
2022-03-30 10:43:15 +0200

    Delete tests directory

--- tests/core/setup.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const WebServer = require('../../src/core/web-server');
-
-module.exports = async () => {
-  const webServer = new WebServer();
-  webServer.start();
-  global.webServer = webServer;
-}
