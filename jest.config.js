872ddc7a03cf04648fdc4ac1601bcb02e2eb070d
zmhadi <93578078+zmhadi@users.noreply.github.com>
2022-03-30 10:44:18 +0200

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  globalSetup: './tests/core/setup.js',
-  globalTeardown: './tests/core/teardown.js',
-  globals: {
-    apiUrl: 'http://localhost:3000',
-  },
-};
-
-module.exports = config;
